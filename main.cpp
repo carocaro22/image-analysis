b6fb0c472317658087ec387f3e9ef7ea4f2cc469
Carolina <cdarocha@web.de>
2024-03-13 21:10:40 +0100

    segmentation fault

--- main.cpp
+++ main.cpp
@@ -5,68 +5,52 @@
 #include <stack>
 #include <list>
 
-std::vector<std::pair<int, int>> indexObject(cv::Mat &img, std::pair<int, int> start, cv::Vec3b th, cv::Vec3b newColor) {
-    std::stack<std::pair<int, int>> stack;
-    std::vector<std::pair<int, int>> pixels;
+std::vector<std::pair<uint, uint>> indexObject(cv::Mat &img, std::pair<uint, uint> start, cv::Vec3b th, cv::Vec3b newColor) {
+    std::stack<std::pair<uint, uint>> stack;
+    std::vector<std::pair<uint, uint>> pixels;
     stack.push(start);
     while (!(stack.empty())) {
         // Current coordinates
         std::pair c = stack.top();
-        stack.pop();
         pixels.push_back(c); 
         img.at<cv::Vec3b>(c.first, c.second) = newColor;
+        stack.pop();
         
-        if (img.at<cv::Vec3b>(c.first + 1, c.second)[0] != th[0] 
-        && img.at<cv::Vec3b>(c.first + 1, c.second)[1] != th[1] 
-        && img.at<cv::Vec3b>(c.first + 1, c.second)[2] != th[2] 
-        && img.at<cv::Vec3b>(c.first + 1, c.second)[0] != newColor[0] 
-        && img.at<cv::Vec3b>(c.first + 1, c.second)[1] != newColor[1] 
-        && img.at<cv::Vec3b>(c.first + 1, c.second)[2] != newColor[2] 
+        if (img.at<cv::Vec3b>(c.first + 1, c.second) != th
+        && img.at<cv::Vec3b>(c.first + 1, c.second) != newColor
         ) {
-            std::pair<int,int> l = std::make_pair(c.first + 1, c.second);
+            std::pair<uint,uint> l = std::make_pair(c.first + 1, c.second);
             stack.push(l);
         }
-        if (img.at<cv::Vec3b>(c.first - 1, c.second)[0] != th[0] 
-        && img.at<cv::Vec3b>(c.first - 1, c.second)[1] != th[1] 
-        && img.at<cv::Vec3b>(c.first - 1, c.second)[2] != th[2] 
-        && img.at<cv::Vec3b>(c.first - 1, c.second)[0] != newColor[0]
-        && img.at<cv::Vec3b>(c.first - 1, c.second)[1] != newColor[1] 
-        && img.at<cv::Vec3b>(c.first - 1, c.second)[2] != newColor[2] 
+        if (img.at<cv::Vec3b>(c.first - 1, c.second) != th
+        && img.at<cv::Vec3b>(c.first - 1, c.second) != newColor
         ) {
-            std::pair<int,int> o = std::make_pair(c.first - 1, c.second);
+            std::pair<uint,uint> o = std::make_pair(c.first - 1, c.second);
             stack.push(o);
         }
-        if (img.at<cv::Vec3b>(c.first, c.second + 1)[0] != th[0] 
-        && img.at<cv::Vec3b>(c.first, c.second + 1)[1] != th[1] 
-        && img.at<cv::Vec3b>(c.first, c.second + 1)[2] != th[2] 
-        && img.at<cv::Vec3b>(c.first, c.second + 1)[0] != newColor[0] 
-        && img.at<cv::Vec3b>(c.first, c.second + 1)[1] != newColor[1] 
-        && img.at<cv::Vec3b>(c.first, c.second + 1)[2] != newColor[2] 
+        if (img.at<cv::Vec3b>(c.first, c.second + 1) != th
+        && img.at<cv::Vec3b>(c.first, c.second + 1) != newColor
         ) {
-            std::pair<int,int> s = std::make_pair(c.first, c.second + 1);
+            std::pair<uint,uint> s = std::make_pair(c.first, c.second + 1);
             stack.push(s);
         }
-        if (img.at<cv::Vec3b>(c.first, c.second - 1)[0] != th[0] 
-        && img.at<cv::Vec3b>(c.first, c.second - 1)[1] != th[1] 
-        && img.at<cv::Vec3b>(c.first, c.second - 1)[2] != th[2] 
-        && img.at<cv::Vec3b>(c.first, c.second - 1)[0] != newColor[0] 
-        && img.at<cv::Vec3b>(c.first, c.second - 1)[1] != newColor[1] 
-        && img.at<cv::Vec3b>(c.first, c.second - 1)[2] != newColor[2]
+        if (img.at<cv::Vec3b>(c.first, c.second - 1) != th
+        && img.at<cv::Vec3b>(c.first, c.second - 1) != newColor
         ) {
-            std::pair<int,int> n = std::make_pair(c.first, c.second - 1);
+            std::pair<uint,uint> n = std::make_pair(c.first, c.second - 1);
             stack.push(n);
         }
     } 
     return pixels;
 }
 
-std::list<std::vector<std::pair<int, int>>> indexAndColor(cv::Mat &img, cv::Vec3b th) {
-    std::list<std::vector<std::pair<int,int>>> objectList;
+std::list<std::vector<std::pair<uint, uint>>> indexAndColor(cv::Mat &img, cv::Vec3b th) {
+    std::list<std::vector<std::pair<uint,uint>>> objectList;
 
-    for (int i = 0; i < img.rows; i++) {
-        for (int j = 0; j < img.cols; j++) {
+    for (uint i = 0; i < img.rows; i++) {
+        for (uint j = 0; j < img.cols; j++) {
             if (img.at<cv::Vec3b>(i,j) == th) {
-                std::pair<int, int> start = std::make_pair(i,j);
+                std::pair<uint, uint> start = std::make_pair(i,j);
                 cv::Vec3b color = cv::Vec3b((rand() % 255), (rand() % 255), (rand() % 255));
                 objectList.push_back(indexObject(img, start, th, color));
             }
@@ -81,7 +65,7 @@ int main(int argc, char** argv)
     cv::Mat img = cv::imread("images/train.png", cv::IMREAD_COLOR);
     cv::Vec3b th = cv::Vec3b(0, 0, 0);
 
-    std::list<std::vector<std::pair<int, int>>> objectList = indexAndColor(img, th); 
+    std::list<std::vector<std::pair<uint, uint>>> objectList = indexAndColor(img, th); 
 
     cv::imshow("Index", img);
     cv::waitKey(0);
